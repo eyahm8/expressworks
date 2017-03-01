31ae797e1997471acaed9c03dcc12b9096e26f32
Mikael Rasimus <mikael.rasimus@gmail.com>
2017-03-01 12:11:26 +0700

    6/8

---
+++ parampam.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.put('/message/:id', function(req, res){
+
+  var id = req.params.id;
+  var shadate = require('crypto')
+      .createHash('sha1')
+      .update(new Date().toDateString() + id)
+      .digest('hex')
+
+      res.send(shadate);
+});
+
+app.listen(process.argv[2]);
